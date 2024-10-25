412b377fde7476e5a5dc7cd74c78a7cb174a9250
unknown <marouanosb@gmail.com>
2024-10-25 10:45:47 +0200

    options2

--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -2,12 +2,11 @@
 fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
-    } else {
-<<<<<<< HEAD
+    } else if fizzish == "fuzz" {
         "bar"
-=======
-        1
->>>>>>> 5f23f738de0a4a2d7d833fd9c870f8b8d71a434f
+    }
+    else {
+        "baz"
     }
 }
 
